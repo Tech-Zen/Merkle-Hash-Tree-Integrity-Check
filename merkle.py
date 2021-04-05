b550278279e4ecfbeb864bc0de9c58e9b59750aa
Isaac B <81873545+Tech-Zen@users.noreply.github.com>
2021-04-05 12:38:01 -0400

    Update merkle.py

--- merkle.py
+++ merkle.py
@@ -1,20 +1,28 @@
 #Merkle Root Algorithim with n file input
-import hashlib
-import os
+#Coded by Isaac Beasley
+#For CIS 555
 
-#Loop to open all files and append them to leaves list
+#importing necessary libraries
+#hashlib  library for hashing functions
+#os allows access to os system dependent functionality
+import hashlib
+import os 
+
+#This Loop opens all files in the listed directory, and appends them to leaves list
+#Change this path to your desired directory to use of this program.
 folderpath = r"C:/Users/isaac/Desktop/merkle files/"
 filepaths = [os.path.join(folderpath, name) for name in os.listdir(folderpath)]
 
-#creates empty list to store hashed SHA-1 files
+#Creates empty list to store hashed SHA-1 files
 leaves = []
 
-#open all files in folderpath, hash the files, and append them to the leaves list
+#Open all files in folderpath, hash the files using Hashlib sha1, and append them to the leaves list
 for path in filepaths:
     with open(path, 'r') as f:
         data = f.read()
         hash = hashlib.sha1(data.encode('utf-8')).hexdigest()
         leaves.append(hash)
+#Prints first row and base layer of Merkle Tree
 print("Blocks hashes:")
 print(leaves)
 
@@ -26,12 +34,15 @@ def print_leaves():
 def is_odd(n):
     return n % 2 == 1
 
+#First it checks if the number of leaves is odd. As we want a binary tree, we will append the last item in the list to make it even.
 while len(leaves) > 1:
     if is_odd(len(leaves)):
         leaves.append(leaves[-1])
         print_leaves()
     i = 0
     j = 0
+    #These loops act to step, and jump through the base Merkle leaves
+    #Once equal pairs are created, they're concatinated and hashed into a parent leaf using SHA-1.
     while j < len(leaves):
         leaves[i] = leaves[j] + leaves[j + 1]
         leaves[i] = hashlib.sha1(leaves[i].encode('utf-8')).hexdigest()
@@ -40,6 +51,7 @@ while len(leaves) > 1:
     leaves = leaves[:len(leaves) // 2]
     print_leaves()
 
+#Assigns leaves list to MerkleRoot, just for easier understanding of code
 MerkleRoot = leaves
 #This prints the Merkle Root
 print('')
